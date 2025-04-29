9fccc467f3b8a99b7950b9b8219032bc5bc22243
klikki-lab <klikki.lab@gmail.com>
2025-04-29 13:54:40 +0900

    Randomクラスの更新

--- src/common/util/random.ts
+++ src/common/util/random.ts
@@ -3,27 +3,50 @@ export class Random {
     constructor(private random: g.RandomGenerator) { }
 
     /** 
-     * @returns 0 以上 1 未満のランダム値を返す。
+     * @returns `0` 以上 `1` 未満のランダム値を返す。
      */
-    generate(): number {
-        return this.random.generate();
-    }
+    generate(): number;
+
+    /** 
+     * @param max 最大値
+     * @returns `0` 以上 `max` 未満のランダム値を返す。
+     */
+    generate(max: number): number;
 
     /**
      * @param min 最小値
      * @param max 最大値
-     * @returns min 以上 max 未満のランダム値を返す。
+     * @returns `min` 以上 `max` 未満のランダム値を返す。
      */
-    range(min: number, max: number): number {
-        return this.generate() * (max - min) + min;
+    generate(min: number, max: number): number;
+
+    generate(arg1?: number, arg2?: number): number {
+        if (arg1 === undefined) {
+            return this.random.generate();
+        }
+        if (arg2 === undefined) {
+            return this.random.generate() * arg1;
+        }
+        return this.random.generate() * (arg2 - arg1) + arg1;
     }
+
+    /** 
+     * @param max 最大値
+     * @returns `0` 以上 `max` 未満のランダム整数を返す。
+     */
+    generateInt(max: number): number;
 
     /**
      * @param min 最小値
      * @param max 最大値
-     * @returns min 以上 max 未満の整数のランダム値を返す。
+     * @returns `min` 以上 `max` 未満のランダム整数を返す。
      */
-    rangeInt(min: number, max: number): number {
-        return Math.floor(this.range(min, max));
+    generateInt(min: number, max: number): number;
+
+    generateInt(arg1?: number, arg2?: number): number {
+        if (arg2 === undefined) {
+            return Math.floor(this.random.generate() * arg1);
+        }
+        return Math.floor(this.random.generate() * (arg2 - arg1) + arg1);
     }
 }
